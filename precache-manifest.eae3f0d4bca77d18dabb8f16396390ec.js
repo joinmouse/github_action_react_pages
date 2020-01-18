50f4648ac8a145839333f0b09e815ded053aae16
joinmouse <misswanqiu@gmail.com>
2020-01-18 08:57:54 +0000

    Deploying to gh-pages from master 06b17ff49e7ed2ecb9fe242f856dd7ff36bcc7ab

---
+++ precache-manifest.eae3f0d4bca77d18dabb8f16396390ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e5c8632ed9534886bd78f460e4f243",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "add004ffacceed774327",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f4165d81d2109ac4b1b4",
+    "url": "/github-actions-demo/static/js/2.b9ea0360.chunk.js"
+  },
+  {
+    "revision": "add004ffacceed774327",
+    "url": "/github-actions-demo/static/js/main.951d89b6.chunk.js"
+  },
+  {
+    "revision": "cddc59b2ec36bd5f3d0c",
+    "url": "/github-actions-demo/static/js/runtime-main.6828db99.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
